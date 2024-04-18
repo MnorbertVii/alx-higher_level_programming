e48417716a483b63217d235400c5c4e52511f338
NIRUGIRA Muhizi Norbert <norbertmuhizi21@gmail.com>
2024-04-17 18:19:28 -0700

    fixed bugs

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Defines a class Student."""
 
 
@@ -24,6 +25,7 @@ class Student:
             attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+                all(type(element) == str for element in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
